06813cd380c6594e85ad28ec26e210264f50cfbd
Alex Holt <berber.it@gmail.com>
2015-11-11 18:03:30 +0200

    - Fixes in DeployScript

--- SystemUtils.py
+++ SystemUtils.py
@@ -57,7 +57,7 @@ class SystemUtils(object):
             print '\n<------------SYSTEM INFO------------>'
             print sys.version
             print 'Your version of Linux incompatible with this script, exit...'
-            print '<------------SYSTEM INFO------------>'
+            print '<------------SYSTEM INFO------------>\n'
             sys.exit()
 
 
